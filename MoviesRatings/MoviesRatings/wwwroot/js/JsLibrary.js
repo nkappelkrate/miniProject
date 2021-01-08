2f37dc2c19597d07f5d9759a8410ece94852355f
hnguyenkrate <h.nguyen@krate.com>
2021-01-08 11:49:39 -0600

    Create Modal component
    create modal component, add button to open the Modal

---
+++ MoviesRatings/MoviesRatings/wwwroot/js/JsLibrary.js
@@ -0,0 +1,20 @@
+﻿//Author: Hieu Nguyen
+
+function showModal(dialog) {
+    console.log("showModal");
+    if (!dialog.open) {
+        dialog.showModal();
+    }
+}
+function initializeModal(dialog, reference) {
+    dialog.addEventListener("close", async e => {
+        //calling C# method
+        await reference.invokeMethodAsync("OnClose", dialog.returnValue);
+    });
+}
+
+function closeModal(dialog) {
+    if (dialog.open) {
+        dialog.close();
+    }
+}
