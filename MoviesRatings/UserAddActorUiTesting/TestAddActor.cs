150dd1ab0b271a7ff9fa8c4d21971c725d918f79
hnguyenkrate <h.nguyen@krate.com>
2021-01-14 12:59:34 -0600

    All UI tests are passed, ready to push to master

--- MoviesRatings/UserAddActorUiTesting/TestAddActor.cs
+++ MoviesRatings/UserAddActorUiTesting/TestAddActor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿/**Author: Hieu Nguyen
+ */
+using System;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
@@ -28,11 +30,10 @@ namespace UserAddActorUiTesting
 
         [TestMethod]
         [TestCategory("Chrome")]
-        public void TestValidAddNewActor()
+        public void Test1_validAddNewActor()
         {            
-            ///navigate to the actor page
-            driver.Navigate().GoToUrl(appURL);         
-            //Wait for 4 second    
+                 
+            //Wait for 2 second    
             WaitFor(2);
             //Calling helper method to add new actor
             AddActor("Morgan", "Freeman", "Male");
@@ -48,21 +49,168 @@ namespace UserAddActorUiTesting
         }
         [TestMethod]
         [TestCategory("Chrome")]
-        public void TestExceptionCreateActorWithoutFirstName()
+        public void Test2_exceptionCreateActorWithoutFirstName()
         {
             AddActor("", "Reynolds", "Male");
-            var modelBtnAdd = wait.Until(e => e.FindElement(By.Id("modelBtnAdd"))); ;
+            var modelBtnAdd = wait.Until(e => e.FindElement(By.Id("modelBtnAdd"))); 
             modelBtnAdd.Click();
-            WaitFor(4);
+            WaitFor(2);
             //check for validation error
             var firstNameError = driver.FindElement(By.Id("firstNameErr"));
             Assert.AreEqual(expectedFirstNameEmptyErrMsg, firstNameError.GetAttribute("textContent"));
         }
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Test3_exceptionCreateActorWithoutLastName()
+        {
+            AddActor("Ryan", "", "Male");
+            var modelBtnAdd = wait.Until(e => e.FindElement(By.Id("modelBtnAdd"))); ;
+            modelBtnAdd.Click();
+            WaitFor(2);
+            //check for validation error
+            var lastNameError = driver.FindElement(By.Id("lastNameErr"));
+            Assert.AreEqual(expectedLastNameEmptyErrMsg, lastNameError.GetAttribute("textContent"));
+
+        }
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Test4_exceptionCreateActorWithoutSelectAgenderType()
+        {
+            AddActor("Ryan", "Renold", "-1");
+            var modelBtnAdd = wait.Until(e => e.FindElement(By.Id("modelBtnAdd"))); ;
+            modelBtnAdd.Click();
+            WaitFor(2);
+            //check for validation error
+            var genderErr = driver.FindElement(By.Id("genderErr"));
+            Assert.AreEqual(expectedGenderRequiredErrMsg, genderErr.GetAttribute("textContent"));
+
+        }
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Test5_validBoundaryFirstNameWithTwoCharacters()
+        {
+
+            AddActor("Ba", "Pitt", "Male");
+            var modelBtnAdd = driver.FindElement(By.Id("modelBtnAdd"));
+            modelBtnAdd.Click();
+            WaitFor(2);
+            //Check for successful added message
+            //Check for the first row's value            
+            var alert = driver.SwitchTo().Alert();
+            Assert.AreEqual(expectedAlertAddedMsg, alert.Text);
+            alert.Accept();
+            //Clean up
+            RemoveActor(3);
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Test6_validBoundaryFirstNameWith50Characters()
+        {
+            //generate a line of repeated a 50 times            
+            AddActor(new string('a', 50), "Damon", "Male");
+            var modelBtnAdd = driver.FindElement(By.Id("modelBtnAdd"));
+            modelBtnAdd.Click();
+            WaitFor(2);
+            //Check for successful added message
+            //Check for the first row's value
+            var alert = driver.SwitchTo().Alert();
+            Assert.AreEqual(expectedAlertAddedMsg, alert.Text);
+            alert.Accept();
+            //Clean up
+            RemoveActor(3);
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Test7_invalidBoundaryFirstNameWithOneCharacters()
+        {
+            AddActor("a", "Bale", "Male");
+            WaitFor(2);
+            //check for validation error
+            var firstNameError = driver.FindElement(By.Id("firstNameErr"));
+            Assert.AreEqual(expectedFirstNameOutOfBoundErrMsg, firstNameError.GetAttribute("textContent"));
+
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Test8_invalidBoundaryFirstNameWith51Characters()
+        {
+
+            AddActor(new string('a', 51), "Hanks", "Male");
+            WaitFor(2);
+            //check for validation error
+            var firstNameError = driver.FindElement(By.Id("firstNameErr"));
+            Assert.IsTrue(firstNameError.Displayed);
+            Assert.AreEqual(expectedFirstNameOutOfBoundErrMsg, firstNameError.GetAttribute("textContent"));
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Test9_validBoundaryLastNameWIthTwoCharacters()
+        {
+            AddActor("Ryan", "Re", "Male");
+            var modelBtnAdd = wait.Until(e => e.FindElement(By.Id("modelBtnAdd"))); ;
+            modelBtnAdd.Click();
+            WaitFor(1);
+            //Check for successful added message
+            var alert = driver.SwitchTo().Alert();
+            Assert.AreEqual(expectedAlertAddedMsg, alert.Text);
+            alert.Accept();
+            //Removing Morgan Freeman
+            RemoveActor(3);
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Test_10_validBoundaryLastNameWIth50Characters()
+        {
+
+            AddActor("Ryan", new string('a', 50), "Male");
+            var modelBtnAdd = wait.Until(e => e.FindElement(By.Id("modelBtnAdd"))); ;
+            modelBtnAdd.Click();
+            WaitFor(1);
+            //Check for successful added message
+            var alert = driver.SwitchTo().Alert();
+            Assert.AreEqual(expectedAlertAddedMsg, alert.Text);
+            alert.Accept();
+            //Removing actor
+            RemoveActor(3);
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Test_11_invalidBoundaryLastNameWIthOneCharacters()
+        {
+
+            AddActor("Denis", "a", "Female");            
+            WaitFor(1);
+            //check for validation error
+            var lastNameError = driver.FindElement(By.Id("lastNameErr"));
+            Assert.IsTrue(lastNameError.Displayed);
+            Assert.AreEqual(expectedLastNameOutOfBoundErrMsg, lastNameError.GetAttribute("textContent"));
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Test_12_invalidBoundaryLastNameWIth51Characters()
+        {
+
+            AddActor("Jessica", new string('A', 51), "Female");
+            WaitFor(1);
+            //check for validation error
+            var lastNameError = driver.FindElement(By.Id("lastNameErr"));
+            Assert.IsTrue(lastNameError.Displayed);
+            Assert.AreEqual(expectedLastNameOutOfBoundErrMsg, lastNameError.GetAttribute("textContent"));
+        }
+
         #region Test Helper methods  
         /*
          * This is a helper method that is used to create new actor
          * fName: actor first name
          * lName: actor last name
+         * genderType: actor gender
          */
         private void AddActor(string fName, string lName, string genderType)
         {
@@ -74,7 +222,7 @@ namespace UserAddActorUiTesting
             WaitFor(2);
             var btnAdd = wait.Until(e => e.FindElement(By.Id("btnAddActor")));            
             btnAdd.Click();
-            System.Threading.Thread.Sleep(4000);
+            WaitFor(1);
             //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
             //wait for the model to loadup properly 
             wait.Until(e => e.FindElement(By.Id("modelBtnAdd")));
@@ -126,8 +274,7 @@ namespace UserAddActorUiTesting
         [TestInitialize()]
         public void SetupTest()
         {
-            appURL = "http://localhost:5000/";
-            //appURL = "http://eaapp.somee.com/";
+            appURL = "http://localhost:5000/";            
             string browser = "chrome";
             switch (browser)
             {
